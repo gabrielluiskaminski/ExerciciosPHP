98c9239f9c44487904ce939879ff05d5f3f84163
Gabriel Luis Kaminski <gabrielluiskaminski@alunos.utfpr.edu.br>
2025-02-19 20:31:40 -0300

    Segunda lista

---
+++ lista2/001.php
@@ -0,0 +1,8 @@
+<?php 
+    $numero = readline("Digite um número: ");
+    if ($numero > 10) {
+        echo "Este número é maior que 10!";
+    } else {
+        echo "Não é maior que 10.";
+    }
+?>
