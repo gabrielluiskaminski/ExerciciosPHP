a6b6c83a5023cba6bd45943ca2809ca6090f7238
Gabriel Luis Kaminski <gabrielluiskaminski@alunos.utfpr.edu.br>
2025-02-20 12:55:05 -0300

    Exercicio dois

---
+++ lista2/002.php
@@ -0,0 +1,8 @@
+<?php 
+    $numero = readline("Digite um número: ");
+    if ($numero >= 0) {
+        echo ("Este número é positivo.");
+    } else {
+        echo ("Este número é negativo.");
+    }
+?>
